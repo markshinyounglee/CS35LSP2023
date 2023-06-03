c8391f8a487cb8882f75a8d15f514da83d80c493
Taejusyee <taejusyee@gmail.com>
2023-06-03 14:26:31 -0700

    reset to working state

---
+++ client/src/components/StartBeef.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const CreateBeef = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="start-beef">
+          <h2>Start A Beef Here</h2>
+          <form className="beef-form" onSubmit={handleSubmit}>
+            <label htmlFor="text">Title</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              placeholder="youremail@gmail.com"
+              id="email"
+              name="email"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="text"
+              placeholder="********"
+              id="password"
+              name="password"
+            />
+            <button type="submit">Log In</button>
+          </form>
+        </div>
+      );
+}
+
+export default CreateBeef
